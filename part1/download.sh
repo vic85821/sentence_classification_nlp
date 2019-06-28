2ffef08abe6ac45906355511b986ffcee8b0b448
vic85821 <vic85821@gmail.com>
2019-06-28 20:17:34 +0800

    Add the corpus pkl download link

--- part1/download.sh
+++ part1/download.sh
@@ -1,3 +1,5 @@
 wget -O model/submission/ckpts/epoch-9.ckpt 'https://gntuedutw-my.sharepoint.com/:u:/g/personal/r07922058_g_ntu_edu_tw/EQsFL9fxBjlKqSCS_o3OieIBft40rDp1s8UutcP8iFhFww?download=1'
 
 wget -O ELMo/models/model.pkl 'https://gntuedutw-my.sharepoint.com/:u:/g/personal/r07922058_g_ntu_edu_tw/EaaMhEDoS5lErk15Fe0nczoB6p5Wvpb9cX_SGsowUnG-8w?download=1'
+
+wget -O ELMo/dataset/corpus.pkl 'https://gntuedutw-my.sharepoint.com/:u:/g/personal/r07922058_g_ntu_edu_tw/EfxtDcOmfdFDr6TOqnCq0V8Brfwrq-pKrQoIkde-0L0fuQ?download=1'
